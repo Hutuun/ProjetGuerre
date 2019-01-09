7a35bb78903b43153325fc627b807ec857133e6b
Hutuun <sullivan1.honnet@gmail.com>
2019-01-09 13:20:43 +0000

    plus de chargement

--- include/Terrain.h
+++ include/Terrain.h
@@ -35,6 +35,7 @@ class Terrain
     private:
         Epoque* chargeEpoque(std::string epoque);
         Soldat* chargeUnite(std::string unite, std::string dir, unsigned int pv, unsigned int pm, unsigned int porte, unsigned int pos, unsigned int prix, unsigned int pa);
+        Batiment* chargeBatiment(std::string batiment, std::string dir);
         void initJoueur(Joueur** j, unsigned int pos);
         static Terrain* m_instance;
         std::vector<Case*> m_terrain;
